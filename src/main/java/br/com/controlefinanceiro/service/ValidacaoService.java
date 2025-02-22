fac8f4235d07c7db35af4701a6804b4e4449d25a
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-02-21 22:17:58 -0300

    adiciona documentação para métodos de validação no ValidacaoService

--- src/main/java/br/com/controlefinanceiro/service/ValidacaoService.java
+++ src/main/java/br/com/controlefinanceiro/service/ValidacaoService.java
@@ -8,11 +8,28 @@ import org.springframework.stereotype.Service;
 public class ValidacaoService 
 {
     
+   /**
+     * Valida o objeto de forma genérica, verificando se o campo identificado como chave primária 
+     * não foi informado durante o cadastro. 
+     * 
+     * @param objeto O objeto que será validado. Pode ser qualquer entidade ou objeto que possua uma chave primária.
+     * @param primaryKey O nome do campo que representa a chave primária no objeto (por exemplo, "id_conta", "id_categoria").
+     * @throws Exception Lança uma exceção caso o campo de chave primária esteja informado no cadastro.
+     */ 
     public void validarCadastroGeral(Object objeto,String primaryKey)  throws Exception
     {
         validaIdNaoInformado(objeto, primaryKey);
     }
 
+    /**
+     * Verifica se o campo identificado como chave primária no objeto contém algum valor. 
+     * Se o valor for diferente de nulo, uma exceção será lançada indicando que o ID 
+     * não pode ser informado durante o cadastro.
+     * 
+     * @param objeto O objeto a ser validado.
+     * @param primaryKey O nome do campo que representa a chave primária no objeto.
+     * @throws Exception Lança uma exceção caso o campo de chave primária esteja informado no cadastro.
+     */
     public void validaIdNaoInformado(Object objeto, String primaryKey) throws Exception
     {
       
@@ -24,6 +41,14 @@ public class ValidacaoService
         }
     }
 
+    /**
+     * Utiliza reflexão para acessar dinamicamente o valor de um campo de um objeto. 
+     * 
+     * @param objeto O objeto do qual o campo será acessado.
+     * @param campo O nome do campo no objeto.
+     * @return O valor do campo solicitado.
+     * @throws Exception Lança exceções se o campo não existir ou se o acesso for ilegal.
+     */
     public Object obterCampo(Object objeto, String campo) throws Exception
     {
         Field field = objeto.getClass().getDeclaredField(campo);
