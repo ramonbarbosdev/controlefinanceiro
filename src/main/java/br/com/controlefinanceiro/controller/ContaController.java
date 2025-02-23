e8f7ab30e149ec892682e05d815a1506674bdb00
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-02-22 21:41:34 -0300

    refatora ContaController para herdar de BaseController, centralizando a lógica de manipulação de dados e inicializando relacionamentos

--- src/main/java/br/com/controlefinanceiro/controller/ContaController.java
+++ src/main/java/br/com/controlefinanceiro/controller/ContaController.java
@@ -1,7 +1,9 @@
 package br.com.controlefinanceiro.controller;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
@@ -25,10 +27,12 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.controlefinanceiro.MensagemException;
 import br.com.controlefinanceiro.DTO.CategoriaDTO;
 import br.com.controlefinanceiro.DTO.ContaDTO;
+import br.com.controlefinanceiro.config.RelacionamentoConfig;
 import br.com.controlefinanceiro.model.Conta;
 import br.com.controlefinanceiro.model.Tipo_Categoria;
 import br.com.controlefinanceiro.model.Tipo_Conta;
 import br.com.controlefinanceiro.repository.ContaRepository;
+import br.com.controlefinanceiro.repository.TipoCategoriaRepository;
 import br.com.controlefinanceiro.repository.TipoContaRepository;
 import br.com.controlefinanceiro.service.SequenciaService;
 import br.com.controlefinanceiro.service.Utils;
@@ -40,186 +44,21 @@ import java.util.stream.Collectors;
 
 @RestController 
 @RequestMapping(value = "/conta", produces = "application/json")
-public class ContaController {
+public class ContaController extends BaseController<Conta, ContaDTO, Long>
+{
 
-	@Autowired
-	private ContaRepository objetoRepository;
-	
-	@Autowired
-	private TipoContaRepository tipoContaRepository;
-	
-	@Autowired
-	private Utils utils;
-
-	@Autowired
-	private ValidacaoService validacaoService ;
-	
-	//atributos
-	private Class<?> model = Conta.class;
-	private String primaryKey = "id_conta";
-	private ModelMapper modelMapper = new ModelMapper();  
-
-	
-	@GetMapping(value = "/{id_conta}")
-	public ResponseEntity<?> obterId(@PathVariable(value ="id_conta") Long id) throws Exception
+	public ContaController(ContaRepository ContaRepository, TipoContaRepository TipoContaRepository)
 	{
-		try
-		{
-			Object objetoModel = this.model.getDeclaredConstructor().newInstance();
-			Class<?> objetoDTO = utils.obterClasseDtoEntidade(this.model);
-
-			CrudRepository repository = utils.obterRepositoryEntidade(objetoModel);
-
-			Optional<?> objeto = repository.findById(id);
-			
-			if(objeto.isEmpty())
-			{
-				throw new MensagemException("Registro não encontrado!");
-			}
-			
-			Object dto = utils.converterDTO(objeto.get(), objetoDTO);
-
-			return new ResponseEntity<Object>( dto, HttpStatus.OK);
-		}
-		catch (Exception e)
-		{
-				throw new MensagemException( e.getMessage());
-		}
-	}
-	
-	@GetMapping(value = "/", produces = "application/json")
-	@CacheEvict(value = "cacheconta", allEntries = true)
-	@CachePut("cacheconta")
-	public ResponseEntity<List<?>> obterTodos() throws Exception
-	{
-		try
-		{
-			Object objetoModel = this.model.getDeclaredConstructor().newInstance();
-			
-			Class<?> objetoDTO = utils.obterClasseDtoEntidade(this.model);
-
-			CrudRepository repository = utils.obterRepositoryEntidade(objetoModel);
-
-			List<Object> objetos = (List<Object>) repository.findAll();
-
-			if (objetos.isEmpty())
-			{
-				throw new MensagemException("Nenhuma conta encontrada!");
-			}
-
-			List<?> listDTO = objetos.stream()
-										.map(objeto -> utils.converterDTO(objeto, objetoDTO))
-										.collect(Collectors.toList());
-
-			return new ResponseEntity<>(listDTO, HttpStatus.OK);
-		}
-		catch (Exception e)
-		{
-				throw new MensagemException( e.getMessage());
-		}
+		super(Conta.class, ContaDTO.class, "id_conta", inicializarRelacionamentos(TipoContaRepository));
 	}
 
-	// @PostMapping(value = "/", produces = "application/json")
-	// public ResponseEntity<?> cadastrar(@RequestBody ContaDTO dto) throws Exception
-	// {
-	// 	try 
-	// 	{		
-	// 		Conta objeto = modelMapper.map(dto, Conta.class);
-			
-	// 	    utils.obterObjetoRelacionamento(objeto,dto,"id_tipoconta", tipoContaRepository, "setTipoConta", Tipo_Conta.class);
-
-	// 		validacaoService.validarCadastroGeral(objeto, primaryKey);
-	
-	//         Conta objetoSalvo = objetoRepository.save(objeto);
-			
-	// 		return new ResponseEntity<Conta>(objetoSalvo, HttpStatus.OK);
-	// 	} 
-	// 	catch (Exception e)
-	// 	{	
-	// 		 throw new MensagemException( e.getMessage());
-	// 	}
-
-	// }
-
-	@PostMapping(value = "/", produces = "application/json")
-	public ResponseEntity<?> cadastro(@RequestBody Object dto) throws Exception
+	private static Map<String, RelacionamentoConfig> inicializarRelacionamentos(TipoContaRepository repository)
 	{
-		try
-		{
-			Object objetoModel = this.model.getDeclaredConstructor().newInstance();
+		Map<String, RelacionamentoConfig> relacionamentos = new HashMap<>();
+
+		relacionamentos.put("id_tipoconta", new RelacionamentoConfig(repository, "setTipoConta", Tipo_Conta.class));
 		
-			Object objetoDTO = utils.obterObjetoDtoEntidade(this.model);
-
-			modelMapper.map(dto, objetoDTO);
-			modelMapper.map(objetoDTO, objetoModel);
-
-			//modificavel
-			utils.obterObjetoRelacionamento(objetoModel,objetoDTO,"id_tipoconta", tipoContaRepository, "setTipoConta", Tipo_Conta.class);
-
-			validacaoService.validarCadastroGeral(objetoModel, this.primaryKey);
-
-			CrudRepository repository = utils.obterRepositoryEntidade(objetoModel);
-			Object objetoSalvo = repository.save(objetoModel);
-
-			return new ResponseEntity<>(objetoSalvo, HttpStatus.CREATED);
-		}
-		catch (Exception e)
-		{
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
+		return relacionamentos;
 	}
-	
-	@PutMapping(value = "/", produces = "application/json")
-	public ResponseEntity<?> atualizar(@RequestBody Object dto) throws Exception
-	{
-		try 
-		{
-			Object objetoModel = this.model.getDeclaredConstructor().newInstance();
-		
-			Object objetoDTO = utils.obterObjetoDtoEntidade(this.model);
-
-			CrudRepository repository = utils.obterRepositoryEntidade(objetoModel);
-
-			modelMapper.map(dto, objetoDTO);
-			modelMapper.map(objetoDTO, objetoModel);
-
-			//modificavel
-			utils.obterObjetoRelacionamento(objetoModel,objetoDTO,"id_tipoconta", tipoContaRepository, "setTipoConta", Tipo_Conta.class);
-
-			Object objetoSalvo = repository.save(objetoModel);
-
-			return new ResponseEntity<Object>(objetoSalvo, HttpStatus.OK);
-		} 
-		catch (Exception e)
-		{	
-			 throw new MensagemException( e.getMessage());
-		}
-
-	}
-	
-	
-	@DeleteMapping(value = "/{id_conta}", produces = "application/text" )
-	public String delete (@PathVariable("id_conta") Long id) throws Exception
-	{
-		try 
-		{
-
-			Object objetoModel = this.model.getDeclaredConstructor().newInstance();
-			
-			CrudRepository repository = utils.obterRepositoryEntidade(objetoModel);
-
-			repository.deleteById(id);
-			
-			return "Registro deletado!";
-
-		} 
-		catch (Exception e)
-		{	
-			throw new MensagemException( e.getMessage());
-		}
-	}
-	
-
-		
 	
 }
